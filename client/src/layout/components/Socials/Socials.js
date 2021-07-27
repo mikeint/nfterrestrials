74aed23b5fee131dff28abbdd3a04ceb1eb69e9f
michael <michael.sansone@hotmail.com>
2021-07-27 19:00:19 -0400

    adding discord and insta socials properly

--- client/src/layout/components/Socials/Socials.js
+++ client/src/layout/components/Socials/Socials.js
@@ -2,11 +2,13 @@ import React from "react"
 import './Socials.scss' 
 
 import discord from '../../files/images/socialImages/discord.png'
+import instagram from '../../files/images/socialImages/instagram.png'
 import twitter from '../../files/images/socialImages/twitter.png'
 
 const Socials = () => 
     <div className="socialsContainer">
-        <a href="https://www.instagram.com/nfterrestrials" target="_blank" rel="noreferrer"><img src={discord} alt="discord" /></a>
+        <a href="https://discord.com/channels/865443747704995861/867307462859817001" target="_blank" rel="noreferrer"><img src={discord} alt="discord" /></a>
+        <a href="https://www.instagram.com/nfterrestrials" target="_blank" rel="noreferrer"><img src={instagram} alt="instagram" /></a>
         <a href="https://twitter.com/nfterrestrials" target="_blank" rel="noreferrer"><img src={twitter} alt="twitter" /></a>
     </div> 
 
