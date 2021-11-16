5db1bc38bb5bfd164456879cca679c6353856c84
Michael Sansone <only.sansone@gmail.com>
2021-11-16 17:54:50 -0500

    just updating the names

--- client/src/layout/components/Team/Team.js
+++ client/src/layout/components/Team/Team.js
@@ -10,14 +10,14 @@ const Team = () =>
             <img src={t1} alt="t1" />
             <div className="teamDesc">
                 <div className="teamName">Sean Dick</div>
-                <div className="teamPosition">Pinky</div>
+                <div className="teamPosition">Overlord</div>
             </div>
         </div>
         <div className="imageContainer">
             <img src={t2} alt="t2" />
             <div className="teamDesc">
                 <div className="teamName">Michael Sansone</div>
-                <div className="teamPosition">The brain</div>
+                <div className="teamPosition">Dreamer</div>
             </div>
         </div>
     </div> 
